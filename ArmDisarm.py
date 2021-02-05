59e60f7c2f2a99ff5445a7838dbae7e447821422
arshah2 <54183315+arshah2@users.noreply.github.com>
2021-02-04 23:19:20 -0600

    Finished Arm and Disarm
    Arm and Disarm commands work perfectly; checked by values.

--- ArmDisarm.py
+++ ArmDisarm.py
@@ -1,52 +1,23 @@
-import time
-# Import mavutil
-from pymavlink import mavutil
-
-
-def wait_heartbeat(m):
-    print("Waiting for APM heartbeat")
-    m.wait_heartbeat()
-    print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_system))
-def arm(m): 
-    m.mav.command_long_send(
-    m.target_system,
-    m.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    1, 21196, 0, 0, 0, 0, 0)
-def disarm(m):
-    m.mav.command_long_send(
-    m.target_system,
-    m.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    0, 0, 0, 0, 0, 0, 0)
-def reboot(m): 
-    m.mav.command_long_send(
-    m.target_system,
-    m.target_component,
-    mavutil.mavlink.MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN,
-    0,
-    1, 0, 0, 0, 0, 0, 0)
+import commands
 try: 
-    master = mavutil.mavlink_connection("COM4")
-    wait_heartbeat(master)
+    master = commands.connect("COM4")
+    commands.wait_heartbeat(master)
     print ("Arming") 
     print (master.messages['HEARTBEAT'].base_mode)
     print (master.messages['HEARTBEAT'].system_status)
     try: 
-        arm(master)
+        commands.arm(master)
     except: 
         print ("disarming failed")
-    wait_heartbeat(master)
+    commands.wait_heartbeat(master)
     print (master.messages['HEARTBEAT'].base_mode)
     print (master.messages['HEARTBEAT'].system_status) 
     print ("disarming")
     try: 
-        disarm(master)
+        commands.disarm(master)
     except: 
         print ("disarming failed")
-    wait_heartbeat(master)
+    commands.wait_heartbeat(master)
     print (master.messages['HEARTBEAT'].base_mode)
     print (master.messages['HEARTBEAT'].system_status) 
 except KeyboardInterrupt:
