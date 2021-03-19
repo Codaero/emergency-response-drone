2565961355a1a182d9b639a082dfed1853ec5954
arshah2 <54183315+arshah2@users.noreply.github.com>
2021-03-19 15:31:37 -0500

    Reverted to original waypoint code (without mission add)

--- waypoint.py
+++ waypoint.py
@@ -1,11 +1,11 @@
+  
 import commands
 import time
 from pymavlink import mavutil
 
 master = commands.connect("COM6")
 commands.wait_heartbeat(master)
-# commands.upload_mission(master, 417957240, -881662980, 5)
-commands.set_mission(master)
+commands.upload_mission(master, 417957240, -881662980, 5)
 
 
 
